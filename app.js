c21f91bd861b4ba6f3f8836c1b6293cc815eed48
Chanuka-dev <chanukasamarasinghe31@gmail.com>
2023-03-29 22:18:06 +0530

    02-loading-product-detail-data

---
+++ app.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// db.execute('SELECT * FROM products')
+//     .then( result => {
+//         console.log(result[0], result[1]);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('5f9ee5151f62bb0fe1715185')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+    app.listen(3000);
+});
